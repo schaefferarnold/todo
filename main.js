3408215839fb10d30fd5229196f2af1f5ba9c3ee
schaefferarnold <schaefferarnold@gmail.com>
2016-08-11 16:06:22 -0700

    add remove listener

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+function removeTodo () {
+  alert("REMOVE!");
+}
+
 $(function () {
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
-    newTodo.find('.todo-text').val('')
-    newTodo.appendTo('.todo-list')
+    newTodo.find('.todo-text').val('');
+    newTodo.find('.todo-checkbox').prop('checked', false);
+    newTodo.find('.todo-remove').on('click', removeTodo);
+    newTodo.appendTo('.todo-list');
+
   });
+  $('.todo-remove').on('click', removeTodo);
 });
