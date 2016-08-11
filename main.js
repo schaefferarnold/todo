e50d7857c12931866788a35d51fa0dfae6921397
schaefferarnold <schaefferarnold@gmail.com>
2016-08-11 16:16:36 -0700

    remove button and line

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-function removeTodo () {
-  alert("REMOVE!");
+function removeTodo (event) {
+  var target = event.currentTarget;
+  $(target).parent().remove();
 }
 
 $(function () {
+  $('.todo-remove').on('click', removeTodo);
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
     newTodo.find('.todo-text').val('');
