61bc517910eb14bf713b902ef723a5a205882614
schaefferarnold <schaefferarnold@gmail.com>
2016-08-18 16:14:13 -0700

    Move todos between sections

--- main.js
+++ main.js
@@ -3,13 +3,32 @@ function removeTodo (event) {
   $(target).parent().remove();
 }
 
+function moveToCompleted (event){
+  var target = event.currentTarget;
+  var todo = $(target).parent();
+  todo.remove();
+  todo.prependTo('#completed-list')
+  todo.find('.todo-remove').on('click', removeTodo)
+  checkbox.on('click', moveToCurrent);
+}
+
+  function moveToCurrent (event){
+    var target = event.currentTarget;
+    var todo = $(target).parent();
+    todo.remove();
+    todo.prependTo('#current-list')
+    todo.find('.todo-remove').on('click', removeTodo)
+    checkbox.on('click', moveToCompleted);
+    $(target).parent().remove().prependTo('#current-list')
+    .find('.todo-checkbox').on('click', moveToCompleted);
+}
 $(function () {
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
     newTodo.removeClass('hide');
     newTodo.find('.todo-remove').on('click', removeTodo);
-    newTodo.appendTo('.todo-list');
+    newTodo.find('.todo-checkbox').on('click', moveToCompleted);
+    newTodo.appendTo('#current-list');
 
   });
-  $('.todo-remove').on('click', removeTodo);
 });
